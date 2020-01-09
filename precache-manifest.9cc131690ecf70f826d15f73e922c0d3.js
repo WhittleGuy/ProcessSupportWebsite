2dd55be29eb35f1dbdc91807eee9025a660067bf
Brandon <bwwmb6@mst.edu>
2020-01-09 15:42:35 -0600

    Updates

---
+++ precache-manifest.9cc131690ecf70f826d15f73e922c0d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23f5c266494914e5f71b376c8f6c0ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e09484af65150b774d",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "ff692d3c5a867483859f",
+    "url": "/static/css/main.7a2484bd.chunk.css"
+  },
+  {
+    "revision": "59e09484af65150b774d",
+    "url": "/static/js/2.d29453a7.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.d29453a7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff692d3c5a867483859f",
+    "url": "/static/js/main.229c17ac.chunk.js"
+  },
+  {
+    "revision": "e98cd086e678c19e86d5",
+    "url": "/static/js/runtime-main.f5656ddd.js"
+  },
+  {
+    "revision": "b7283a5ef11fd59440f20b84f37acc0b",
+    "url": "/static/media/Brandon.b7283a5e.png"
+  },
+  {
+    "revision": "0a097c751af07b7e9307c67d073bb5ec",
+    "url": "/static/media/Luna.0a097c75.png"
+  },
+  {
+    "revision": "11286eeb7b8e139c895018a7a64f010e",
+    "url": "/static/media/PS.11286eeb.svg"
+  },
+  {
+    "revision": "b80d316c423fffd9b0f1023dfc7cdadd",
+    "url": "/static/media/Paige.b80d316c.png"
+  },
+  {
+    "revision": "b5bb772796b03a2a383c7423323a251d",
+    "url": "/static/media/processsupport.b5bb7727.svg"
+  }
+]);
